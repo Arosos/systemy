696c51aedd5ea74461f51e822c692f9772c1e9bd
Arosos <a.wierzbicki96@gmail.com>
2018-03-29 12:52:25 +0200

    Last change

--- processing.c
+++ processing.c
@@ -1,36 +1,48 @@
 #include "board.h"
 
-int update(int **old_board, int **new_board, int x, int y, int i){
+int update(int **old_board, int **new_board, int x, int y, int step)
+{
     int i, j, count;
-    for (i = 0; i < x; i++){
-        for (j = 0; j < y; j++){
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j < y; j++)
+        {
             count = count_neighbors(old_board, x, y, i, j);
-            if (old_board[i][j] == 1 && count <= 1){
+            if (old_board[i][j] == 1 && count <= 1)
+            {
                 new_board[i][j] = 0;
             }
-            else if (old_board[i][j] == 1 && count >= 4){
+            else if (old_board[i][j] == 1 && count >= 4)
+            {
                 new_board[i][j] = 0;
             }
-            else if (old_board[i][j] == 1 && (count == 2 || count == 3)){
+            else if (old_board[i][j] == 1 && (count == 2 || count == 3))
+            {
                 new_board[i][j] = 1;
             }
-            else if (old_board[i][j] == 0 && count == 3){
+            else if (old_board[i][j] == 0 && count == 3)
+            {
                 new_board[i][j] = 1;
             }
-            else {
+            else
+            {
                 new_board[i][j] = 0;
             }
         }
     }
-	printf("Krok#%d:\n", i);
+    printf("Krok#%d:\n", step);
     print_matrix(new_board, x, y);
 }
 
-int count_neighbors(int **board, int rows, int columns, int x, int y){
+int count_neighbors(int **board, int rows, int columns, int x, int y)
+{
     int i, j, count = 0;
-    for (i = x - 1; i <= x + 1; i++){
-        for (j = y - 1; j <= y + 1; j++){
-            if ((i != x || j != y) && i >= 0 && i < rows && j >= 0 && j < columns){
+    for (i = x - 1; i <= x + 1; i++)
+    {
+        for (j = y - 1; j <= y + 1; j++)
+        {
+            if ((i != x || j != y) && i >= 0 && i < rows && j >= 0 && j < columns)
+            {
                 count += board[i][j];
             }
         }
